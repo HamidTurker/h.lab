a0a01f5c6de6ad1090db3772f9a4e16b0223357d
Hamid Turker <hamidturker@gmail.com>
2023-08-09 20:22:56 -0400

    Add files via upload

---
+++ legacy/h.machinelearning.cost-v0.1.R
@@ -0,0 +1,62 @@
+# Source
+message("h.machinelearning.cost :: v0.1: 2023 Aug 8")
+
+# Function
+h.machinelearning.cost <- function(x, y, w, b, method = "descent") {
+
+  "Compute the cost function for linear regression.
+    
+    Args:
+      x           : Predictive values (rows = examples, columns = features)
+      y           : Predicted, target values (single vector/column)
+      w,b (scalar): Model parameters  𝑓𝑤,𝑏(𝑥)=𝑤𝑥+𝑏 (with either single or multiple predictors 𝑤*𝑥)
+      method      : Method of computation ('descent' [batch gradient descent] or 'normal' [normal equation])
+    
+    Returns:
+      total_cost (float): The cost of using w,b as the parameters for linear regression
+                          to fit the data points y using weighted transformations of x"
+  
+
+  # What method of computation are we using?
+  if (method == "descent") {
+    
+    # Single predictor/feature
+    if (is.null(dim(x))) {
+      
+      # Setup
+      n_examples = length(x)
+      if (!is.numeric(x) | !is.numeric(y) ) { stop("For single feature cost computation, both x and y need to be numeric vectors.") }
+      
+      # Compute sum of all squared errors
+      cost_sum = 0
+      for (i in 1:n_examples) {
+        f_wb_i = w * x[i] + b         # Product of ith example plus bias
+        error = (f_wb_i - y[i]) ** 2  # Error of ith example's predicted score
+        cost_sum = cost_sum + error   # Add ith error to total cost sum
+      }
+    }
+    
+    # Multiple predictors/features
+    if (!is.null(dim(x))) {
+      
+      # Setup
+      n_examples = dim(x)[1]
+      if (!is.data.frame(x) | !is.numeric(y) ) { stop("For multiple feature cost computation, x should be a data frame and y should be a numeric vector.") }
+      
+      # Compute sum of all squared errors
+      cost_sum = 0
+      for (i in 1:n_examples) {
+        f_wb_i = sum(x[i,] * w) + b   # Dot product of ith example plus bias
+        error = (f_wb_i - y[i]) ** 2  # Error for ith example's predicted score
+        cost_sum = cost_sum + error   # Add ith error to total cost sum
+      }
+    }
+    
+    # Compute average total cost and return
+    total_cost = cost_sum / (2*n_examples)
+    return(total_cost)
+    
+  }
+  
+  if (method == "normal") { "Computation with the normal equation hasn't been implemented yet. Sorry!" }
+}
---
+++ legacy/h.machinelearning.gradient-v0.1.R
@@ -0,0 +1,63 @@
+# Source
+message("h.machinelearning.gradient :: v0.1: 2023 Aug 8")
+
+# Function
+h.machinelearning.gradient <- function(x, y, w, b, method = NULL) {
+
+  "Compute the gradient for linear regression.
+  
+    Args:
+      x               : Predictive values (rows = examples, columns = features)
+      y               : Predicted, target values (single vector/column)
+      w,b (scalar)    : Model parameters  𝑓𝑤,𝑏(𝑥)=𝑤𝑥+𝑏 (with either single or multiple predictors 𝑤*𝑥)
+      method (char)   : Method of computation
+  
+    Returns:
+      dj_dw (scalar)  : The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar)  : The gradient of the cost w.r.t. the parameter b"
+  
+  
+  # Single predictor/feature
+  if (is.null(dim(x))) {
+    
+    # Setup
+    n_examples = length(x)
+    dj_dw = 0
+    dj_db = 0
+    
+    # Compute gradient
+    for (i in 1:n_examples) {  
+      f_wb_i = w * x[i] + b             # Regression model with provided parameters
+      dj_dw_i = (f_wb_i - y[i]) * x[i]  # Error * ith feature (part of the partial derivative)
+      dj_db_i = f_wb_i - y[i]           # Error for b
+      dj_dw = dj_dw + dj_dw_i           # Update w and b
+      dj_db = dj_db + dj_db_i
+    }
+  }
+  
+  # Multiple predictors/features
+  if (!is.null(dim(x))) {
+    
+    # Setup
+    n_examples = dim(x)[1]
+    n_features = dim(x)[2]
+    dj_dw = array(0, c(1,n_features))
+    dj_db = 0
+    
+    # Compute gradient
+    for (i in 1:n_examples) {
+      f_wb_i = sum(x[i,] * w) + b   # Model prediction with current weights and bias
+      error = f_wb_i - y[i]         # Error from the model with n_features on the ith example
+      for (j in 1:n_features) {
+        dj_dw[j] = dj_dw[j] + error * x[i,j]
+      }
+      dj_db = dj_db + error
+    }
+  }
+  
+  # Compute average over n_examples and return
+  dj_dw = dj_dw / n_examples 
+  dj_db = dj_db / n_examples
+  return(list(dj_dw, dj_db))
+    
+}
